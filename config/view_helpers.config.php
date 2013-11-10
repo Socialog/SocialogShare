93194ee183d808a3c5407d3b5feeb5f296338d9d
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-11-10 15:30:30 +0100

    added twitter

--- config/view_helpers.config.php
+++ config/view_helpers.config.php
@@ -3,13 +3,16 @@
 namespace SocialogShare;
 
 use SocialogShare\View\Helper\Facebook;
+use SocialogShare\View\Helper\Twitter;
 use Zend\View\HelperPluginManager;
 
 return [
     'factories' => [
         'facebookbutton' => function(HelperPluginManager $sm) {
-            $sl = $sm->getServiceLocator();
             return new Facebook();
+        },
+        'twitterbutton' => function(HelperPluginManager $sm) {
+            return new Twitter();
         }
     ]
 ];
